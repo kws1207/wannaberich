51f61944be0e6fc2c17c0e7bccd097ecd37b57e6
김우성 <kws@gim-useong-ui-MacBookPro.local>
2021-02-21 21:42:10 +0900

    Sidebar complete

--- gony/samjun.py
+++ gony/samjun.py
@@ -1,12 +1,33 @@
 import streamlit as st
 import pandas_datareader as pdr
+from datetime import *
 
-st.write('''
-# 삼성전자 주식 데이터
-마감 가격과 거래량을 차트로 보여줍니다!
-''')
+stocks = {
+    '애플': {'market': 'NASDAQ', 'code': 'AAPL'},
+    '보잉': {'market': 'NYSE', 'code': 'BA'},
+    '나이키': {'market': 'NYSE', 'code': 'NKE'}
+}
 
-df = pdr.get_data_yahoo('005930.KS', '2020-01-01', '2020-09-30')
+terms = {
+    '1개월': timedelta(days=-30),
+    '3개월': timedelta(days=-91),
+    '6개월': timedelta(days=-182),
+    '1년': timedelta(days=-365),
+    '2년': timedelta(days=-730),
+    '5년': timedelta(days=-1461),
+    '10년': timedelta(days=-3652)
+}
+
+st.write('# 부자가 되고싶니')
+
+st.sidebar.header('Menu')
+stock = st.sidebar.selectbox('Stocks', list(stocks.keys()))
+print(stock)
+
+term = st.sidebar.selectbox('Terms', list(terms.keys()))
+
+df = pdr.get_data_yahoo(stocks[stock]['code'],
+                        datetime.today() + terms[term], datetime.today())
 
 st.line_chart(df.Close)
 st.line_chart(df.Volume)

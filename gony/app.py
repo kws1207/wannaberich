dc0d0631a437bf60d8d14e17196f003cf9b5b8bf
김우성 <kws@gim-useong-ui-MacBookPro.local>
2021-02-23 16:18:40 +0900

    FinanceDataReader applied

--- gony/app.py
+++ gony/app.py
@@ -2,16 +2,13 @@ import streamlit as st
 import pandas as pd
 import pandas_datareader as pdr
 from datetime import *
+import FinanceDataReader as fdr
 
-stocks_df = pd.read_html(
-    'http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13', header=0)[0]
-stocks_df.종목코드 = stocks_df.종목코드.map('{:06d}'.format)
-stocks_df = stocks_df[['회사명', '종목코드']]  # 한글로된 컬럼명을 영어로 바꿔준다
-stocks_df = stocks_df.rename(columns={'회사명': 'name', '종목코드': 'code'})
+st.write("""# 부자가 되고싶니~?
 
-# stocks_df = pd.DataFrame.from_dict(stocks, orient='index')
-stocks = stocks_df.to_dict()
-stocks_list = stocks_df['name'].tolist()
+""")
+
+markets = ['S&P500', 'NYSE', 'NASDAQ', 'KOSPI', 'KOSDAQ', 'AMEX']
 
 terms = {
     '1개월': timedelta(days=-30),
@@ -23,14 +20,23 @@ terms = {
     '10년': timedelta(days=-3652)
 }
 
-st.write('# 부자가 되고싶니')
+st.sidebar.header('아수라발발타...')
 
-st.sidebar.header('Menu')
-stock = st.sidebar.selectbox('Stocks', stocks_list)
+market = st.sidebar.selectbox('시장을 골라보렴', markets)
 
-term = st.sidebar.selectbox('Terms', list(terms.keys()))
+stocks_df = fdr.StockListing(market)
+stocks_df = stocks_df[['Name', 'Symbol']]
 
-df = pdr.get_data_yahoo(stocks_df.iloc[stocks_list.index(stock)].loc['code'] + '.KS',
+# stocks_df = pd.DataFrame.from_dict(stocks, orient='index')
+stocks = stocks_df.to_dict()
+stocks_list = stocks_df['Name'].tolist()
+
+stock = st.sidebar.selectbox('종목을 골라보렴', stocks_list)
+
+
+term = st.sidebar.selectbox('기간을 골라보렴', list(terms.keys()))
+
+df = pdr.get_data_yahoo(stocks_df.iloc[stocks_list.index(stock)].loc['Symbol'],
                         datetime.today() + terms[term], datetime.today())
 
 st.line_chart(df.Close)

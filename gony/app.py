fb9483dde5479c179573128e22551fe2e4e867bc
김우성 <kws@gim-useong-ui-MacBookPro.local>
2021-02-23 15:28:02 +0900

    Small change

--- gony/app.py
+++ gony/app.py
@@ -6,7 +6,7 @@ from datetime import *
 stocks_df = pd.read_html(
     'http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13', header=0)[0]
 stocks_df.종목코드 = stocks_df.종목코드.map('{:06d}'.format)
-stocks_df = stocks_df[['회사명', '종목코드']]  # 한글로된 컬럼명을 영어로 바꿔준다.
+stocks_df = stocks_df[['회사명', '종목코드']]  # 한글로된 컬럼명을 영어로 바꿔준다
 stocks_df = stocks_df.rename(columns={'회사명': 'name', '종목코드': 'code'})
 
 # stocks_df = pd.DataFrame.from_dict(stocks, orient='index')
